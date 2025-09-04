e7d7e45fb1aff3886d17007f9ab6c43775da2cd8
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-09-04 14:07:43 +0100

    feat: update server

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,23 @@ import bodyParser from 'body-parser';
 import session from 'express-session';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
+import passport from 'passport';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import path from 'path';
 
 import rateLimitMiddleware from './middleware/rateLimitMiddleware.js';
+import graphqlMiddlewareHandler from './graphql/graphqlHTTPHandler.js';
+import authRouter from './routes/authRoutes.js';
+import { connectDB } from './config/db.js';
+import configureSocket from './config/socket.js';
 
 dotenv.config();
 
 const app = express();
+// const filename = fileURLToPath(import.meta);
+console.log(path.resolve(import.meta.dirname, '..', 'frontend', 'index.html'));
+const indexFilePath = path.resolve(import.meta.dirname, '..', 'frontend');
 
 const sessionOptions = {
   name: 'user_session',
@@ -22,12 +33,47 @@ const sessionOptions = {
     httpOnly: true
   }
 };
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+
+export const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(false, new Error('Domain not supported'));
+    }
+  },
+  method: ['GET', 'POST'],
+  allowedHeaders: ['Authorization', 'Content-Type'],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
+app.use(cookieParser());
 app.use(session(sessionOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 
-app.get('/api', (req, res, next) => {
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(indexFilePath));
+
+// Handles rate limits per user
+app.use(rateLimitMiddleware);
+
+// Handles all auth routes
+app.use('/proxy/auth', authRouter);
+
+// Handles all graphql queries and mutations
+app.use('/graphql', graphqlMiddlewareHandler);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(indexFilePath, 'index.html'));
+});
+
+app.get('/proxy/api', (req, res, next) => {
   try {
     if (!req.session.no) {
       req.session.no = 0;
@@ -40,13 +86,22 @@ app.get('/api', (req, res, next) => {
   }
 });
 
-app.use(rateLimitMiddleware);
+configureSocket(app, corsOptions);
 
 app.use((req, res, next) => {
   console.log('session no is ', req.session.no);
-  res.json({ message: 'Proxy is activated.', no: req.session.no });
+  res.json({ message: 'Proxy is activated.', user: req.user, no: req.session.no });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  next(err || { message: 'Something went wrong' });
 });
 
 const PORT = process.env.PORT || 3302;
 
-app.listen(PORT, () => console.log(`Server is listening at PORT ${PORT}`));
+connectDB(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server is listening at PORT ${PORT}`));
+  })
+  .catch((err) => console.error(err));
