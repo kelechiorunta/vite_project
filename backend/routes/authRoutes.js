bf7d6180900eac8d7abbe5345c85fe467bf5dd33
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-09-04 14:11:57 +0100

    feat: update routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,16 +1,25 @@
 import express from 'express';
 import passport from 'passport';
 
-import { configureLocalPassport } from '../config/passport.js';
+import { configureLocalPassport, configureGooglePassport } from '../config/passport.js';
 import { isauthenticated } from '../middleware/isAuthenticatedMiddleware.js';
-import { logoutController, passportLogin } from '../controllers/authControllers.js';
+import {
+  logoutController,
+  passportLogin,
+  passportRedirect,
+  passportSignup
+} from '../controllers/authControllers.js';
 
 const authRouter = express.Router();
 
 configureLocalPassport(passport);
+configureGooglePassport(passport);
 
 authRouter.get('/logout', logoutController);
 authRouter.post('/signin', passportLogin);
+authRouter.post('/signup', passportSignup);
+authRouter.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+authRouter.get('/oauth2/redirect/google', passport.authenticate('google'), passportRedirect);
 
 authRouter.get('/isAuthenticated', isauthenticated, (req, res) => {
   try {
