c0ee64266e473648b2aef5ce08348bf72e8ebe31
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-10-09 16:10:22 +0100

    tests: implement api e2e testing

---
+++ cypress/support/commands.js
@@ -0,0 +1,48 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+Cypress.Commands.add('login', (email, password) => {
+  cy.session(
+    [email],
+    () => {
+      cy.visit('http://localhost:5173');
+      cy.get('input[name=email]').type(email);
+      cy.get('input[name=email]').should('have.value', email);
+      cy.get('input[name=password]').type(password);
+      cy.get('input[name=password]').should('have.value', password);
+      cy.get('form').contains('button', 'Sign in').click();
+    },
+    {
+      validate: () => {
+        cy.intercept('GET', '/proxy/auth/isAuthenticated', (req) => {
+          req.reply({ statusCode: 200 });
+        }).as('isAuthenticated');
+
+        cy.visit('http://localhost:5173');
+
+        cy.wait('@isAuthenticated').its('response.statusCode').should('eq', 200);
+      }
+    }
+  );
+});
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
