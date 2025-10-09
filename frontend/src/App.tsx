a31e97024500a5876478dfcbe5f23a4f49cbbb92
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-10-09 16:19:35 +0100

    fix: fix import path in e2e file and update ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,7 @@
-import { useEffect } from 'react';
-
-import toast from 'react-hot-toast';
-
-// import { Flex, Text, Button, Grid, Switch, TextArea, Card, Box } from '@radix-ui/themes';
-// import { HomeIcon, ArrowUpIcon } from '@radix-ui/react-icons';
-// import { useTheme } from './components/theme-context';
-
-import Login from './components/Login/Login';
-
 import './App.css';
 import { Route, Routes } from 'react-router';
+
+import Login from './components/Login/Login';
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 import Home from './components/Home/Home';
 import Signup from './components/Signup/Signup';
@@ -17,63 +9,10 @@ import Signup from './components/Signup/Signup';
 function App() {
   // const { toggleTheme, appTheme } = useTheme();
 
-  useEffect(() => {
-    const getProxy = async () => {
-      try {
-        const response = await fetch('/proxy/api', {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        });
-
-        if (!response.ok) {
-          const errorResponse = await response.json();
-          console.log(errorResponse);
-          throw new Error(JSON.stringify(errorResponse));
-        }
-        const data = await response.json();
-        console.log(data);
-      } catch (err: unknown) {
-        console.error(err);
-        if (err instanceof Error) {
-          try {
-            // Try parsing backend error
-            const parsed = JSON.parse(err.message);
-            console.log(parsed);
-            toast.error(parsed?.error ?? 'Something went wrong');
-          } catch {
-            console.error(err.message);
-            // toast.error(err.message || 'An unexpected error occurred');
-          }
-        } else {
-          console.error('Error');
-          // toast.error('Error');
-        }
-      }
-    };
-    getProxy();
-  }, []);
-
   return (
     <Routes>
       <Route element={<ProtectedRoute />}>
-        {/* <Route
-          path="*"
-          element={
-            <div className="App">
-              <App />
-            </div>
-          }
-        /> */}
-        <Route
-          path="/"
-          element={
-            // <div className="App">
-            <Home />
-            // </div>
-          }
-        />
+        <Route path="/" element={<Home />} />
       </Route>
 
       <Route path="/login" element={<Login />} />
