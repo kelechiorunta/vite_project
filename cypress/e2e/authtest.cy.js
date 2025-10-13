d1efcc9dbefac0492cfb26d541825a195242228b
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-10-13 01:14:32 +0100

    test: refactor session creation before all tests

--- cypress/e2e/authtest.cy.js
+++ cypress/e2e/authtest.cy.js
@@ -1,38 +1,19 @@
 describe('authentication test', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:5173', {
+    cy.login(Cypress.env('TEST_EMAIL'), Cypress.env('TEST_PASSWORD'), {
       onBeforeLoad(win) {
         cy.spy(win, 'fetch');
       }
     });
   });
 
-  it('tests the initial api for protected routes', () => {
-    // Confirms the window fetch makes a get request to the route /proxy/auth/isAuthenticated for protected routes
-    cy.window().its('fetch').should('be.calledWith', '/proxy/auth/isAuthenticated');
-    // Confirms the window fetch is called twice initially
-    cy.window().its('fetch').should('have.been.calledTwice');
-    // Confirms the login page
-    cy.url().should('include', '/login');
+  // Confirm server api login request and session cookie
+  it('tests for the credentials command for server signin api request and session cookie', () => {
+    cy.credentials(Cypress.env('TEST_EMAIL'), Cypress.env('TEST_PASSWORD'));
   });
 
-  it('tests the email and password and initial graphql apis query and cookies', () => {
-    cy.url().should('include', '/login');
-
-    // Simulate typing the test email and password
-    cy.get('input[name=email]').type(Cypress.env('TEST_EMAIL'));
-    cy.get('input[name=email]').should('have.value', Cypress.env('TEST_EMAIL'));
-    cy.get('input[name=password]').type(Cypress.env('TEST_PASSWORD'));
-    cy.get('input[name=password]').should('have.value', Cypress.env('TEST_PASSWORD'));
-    cy.get('form').contains('button', 'Sign in').click();
-    cy.url().should('include', '/');
-    // Expect apollo graphql client api queries in browser fetch
-    cy.window().its('fetch').should('be.calledWith', 'http://localhost:3302/graphql');
-    // Expect sessions cookies in browser after login
-    cy.getCookie('user_session').should('exist');
-  });
-
-  it('tests signin api request and interception to expect session cookies', () => {
+  //Confirms browser login and interception
+  it('tests browser login request and interception', () => {
     // Intercept the signin post request
     cy.intercept('POST', 'http://localhost:5173/proxy/auth/signin', {
       body: {
@@ -41,26 +22,24 @@ describe('authentication test', () => {
       }
     }).as('signin');
 
-    // Simulate a api post request for interception and return the expected response and cookies
-    cy.request('POST', 'http://localhost:5173/proxy/auth/signin', {
-      email: Cypress.env('TEST_EMAIL'),
-      password: Cypress.env('TEST_PASSWORD')
-    })
-      .then((response) => {
-        expect(response.status).to.eq(200);
-      })
-      .then(() => cy.getCookie('user_session').should('exist'));
-  });
-
-  it('tests session login command', () => {
-    //  Execute the Login command
-    cy.login(Cypress.env('TEST_EMAIL'), Cypress.env('TEST_PASSWORD'));
+    // Simulate a api post request for interception on the browser context and return the expected response and cookies
     cy.window().then((win) => {
-      cy.spy(win, 'fetch').as('fetchSpy');
-      expect(win.fetch).to.be.calledBefore;
+      return win.fetch('/proxy/auth/signin', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          email: Cypress.env('TEST_EMAIL'),
+          password: Cypress.env('TEST_PASSWORD')
+        })
+      });
+    });
+
+    cy.wait('@signin', { timeout: 1000 }).then((res) => {
+      expect(res.response.statusCode).to.eq(200);
     });
   });
 
+  // Mock Google OAuth
   it('should intercept and mock Google OAuth request', () => {
     // intercept the request to the backend route that triggers Google OAuth
     cy.intercept('GET', '/proxy/auth/google', {
@@ -126,5 +105,9 @@ describe('authentication test', () => {
       const exceededCalls = calls.filter((req) => req.response?.statusCode === 429);
       expect(exceededCalls.length).to.be.greaterThan(0);
     });
+  });
+
+  it('navigate to home page', () => {
+    cy.visit('http://localhost:5173');
   });
 });
