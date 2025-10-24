11fb1a609e348eea9c6c6296c5d258631bdeeebb
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-10-24 01:25:08 +0100

    fix: remove loading actions for new messages

--- frontend/src/components/ChatBody/ChatBody.tsx
+++ frontend/src/components/ChatBody/ChatBody.tsx
@@ -1,6 +1,6 @@
 // ChatBody.tsx
 import * as React from 'react';
-import { Flex, Text, TextField, IconButton, Avatar, Spinner } from '@radix-ui/themes';
+import { Flex, Text, TextField, IconButton, Avatar } from '@radix-ui/themes';
 import { PaperPlaneIcon } from '@radix-ui/react-icons';
 import { ChatHeader } from '../ChatHeader/ChatHeader';
 import type { AuthContextType, ChatMessage, Message } from '../Home/Home';
@@ -25,7 +25,7 @@ const ChatBody: React.FC<{
   authUser,
   messages,
   isMobile,
-  loadingChats,
+  // loadingChats,
   handleSend,
   input,
   handleInput,
@@ -58,89 +58,92 @@ const ChatBody: React.FC<{
         }}
         gap="3"
       >
-        {loadingChats ? (
-          <div
-            style={{
-              display: 'flex',
-              // inset: 0,
-              justifyContent: 'center',
-              alignItems: 'center',
-              width: '100%',
-              minHeight: '60vh',
-              margin: 'auto',
-              color: !appTheme ? 'white' : 'black'
-              //   backgroundColor: 'rgba(0, 0, 0, 0.5)'
-            }}
-          >
-            <Spinner size={'3'} style={{ scale: 4.5 }} />
-          </div>
-        ) : (
+        {
+          //   loadingChats ? (
+          //   <div
+          //     style={{
+          //       display: 'flex',
+          //       // inset: 0,
+          //       justifyContent: 'center',
+          //       alignItems: 'center',
+          //       width: '100%',
+          //       minHeight: '60vh',
+          //       margin: 'auto',
+          //       color: !appTheme ? 'white' : 'black'
+          //       //   backgroundColor: 'rgba(0, 0, 0, 0.5)'
+          //     }}
+          //   >
+          //     <Spinner size={'3'} style={{ scale: 4.5 }} />
+          //   </div>
+          // ) :
           messages &&
-          (messages as ChatMessage[] | Message[] | null)?.map((m) => (
-            <Flex
-              key={typeof m._id === 'string' ? m._id : '?'}
-              justify={
-                (typeof m.sender === 'object' &&
-                  m.sender !== null &&
-                  '_id' in m.sender &&
-                  m.sender?._id === authUser?._id) ||
-                m.sender === authUser?._id
-                  ? 'end'
-                  : 'start'
-              }
-              gapX={'2'}
-            >
-              <Avatar
-                src={
-                  typeof m.sender === 'object' &&
-                  m.sender !== null &&
-                  'picture' in m.sender &&
-                  m.sender?._id === authUser?._id
-                    ? m.sender?.picture
-                    : m.receiverAvatar === 'string' &&
-                        m.receiverAvatar !== null &&
-                        'receiverAvatar' in m
-                      ? m.receiverAvatar
-                      : m.senderAvatar
+            (messages as ChatMessage[] | Message[] | null)?.map((m) => (
+              <Flex
+                key={typeof m._id === 'string' ? m._id : '?'}
+                justify={
+                  (typeof m.sender === 'object' &&
+                    m.sender !== null &&
+                    '_id' in m.sender &&
+                    m.sender?._id === authUser?._id) ||
+                  m.sender === authUser?._id
+                    ? 'end'
+                    : 'start'
                 }
-                fallback={
-                  typeof m.sender === 'object' &&
-                  m.sender !== null &&
-                  'username' in m.sender &&
-                  typeof m.sender?.username === 'string'
-                    ? m.sender.username[0]
-                    : m.receiverName === 'string' && m.receiverName !== null && 'receiverName' in m
-                      ? m.receiverName[0]
-                      : m.senderName !== undefined
-                        ? m.senderName[0]
-                        : '?'
-                }
-                radius="full"
-                size="3"
-              />
-              <Text
-                size="2"
-                style={{
-                  padding: '8px 12px',
-                  borderRadius: '12px',
-                  background:
-                    (typeof m.sender === 'object' &&
-                      m.sender !== null &&
-                      '_id' in m.sender &&
-                      m.sender?._id === authUser?._id) ||
-                    m.sender === authUser?._id
-                      ? 'var(--cyan-5)'
-                      : 'var(--gray-4)',
-                  maxWidth: '70%'
-                }}
+                gapX={'2'}
               >
-                {m.content}
+                <Avatar
+                  src={
+                    typeof m.sender === 'object' &&
+                    m.sender !== null &&
+                    'picture' in m.sender &&
+                    m.sender?._id === authUser?._id
+                      ? m.sender?.picture
+                      : m.receiverAvatar === 'string' &&
+                          m.receiverAvatar !== null &&
+                          'receiverAvatar' in m
+                        ? m.receiverAvatar
+                        : m.senderAvatar
+                  }
+                  fallback={
+                    typeof m.sender === 'object' &&
+                    m.sender !== null &&
+                    'username' in m.sender &&
+                    typeof m.sender?.username === 'string'
+                      ? m.sender.username[0]
+                      : m.receiverName === 'string' &&
+                          m.receiverName !== null &&
+                          'receiverName' in m
+                        ? m.receiverName[0]
+                        : m.senderName !== undefined
+                          ? m.senderName[0]
+                          : '?'
+                  }
+                  radius="full"
+                  size="3"
+                />
+                <Text
+                  size="2"
+                  style={{
+                    padding: '8px 12px',
+                    borderRadius: '12px',
+                    background:
+                      (typeof m.sender === 'object' &&
+                        m.sender !== null &&
+                        '_id' in m.sender &&
+                        m.sender?._id === authUser?._id) ||
+                      m.sender === authUser?._id
+                        ? 'var(--cyan-5)'
+                        : 'var(--gray-4)',
+                    maxWidth: '70%'
+                  }}
+                >
+                  {m.content}
 
-                {<div ref={chatEndRef} />}
-              </Text>
-            </Flex>
-          ))
-        )}
+                  {<div ref={chatEndRef} />}
+                </Text>
+              </Flex>
+            ))
+        }
         {[...typingUsers].length > 0 && typingUsers.has(contact?._id as string) && (
           <div ref={chatEndRef} className="typing-container">
             <Avatar
