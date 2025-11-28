2a5c9d15cbe89a19bd299e3d5bb6cf6912f671fa
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-11-28 22:16:14 +0100

    fix: fix login ui width

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -24,71 +24,78 @@ function App() {
       <Route
         path="/login"
         element={
-          <AlertDialog.Root defaultOpen>
-            <AlertDialog.Content
-              width={{ initial: '100%', xs: '90%', sm: '90%', md: '75%', lg: '95%' }}
-              maxWidth={{ initial: '100%', xs: '90%', sm: '90%', md: '75%', lg: '95%' }}
-              minWidth={{ initial: '120%', xs: '90%', sm: '90%', md: '75%', lg: '95%' }}
-              maxHeight={{ initial: '100%', xs: '90%', sm: '90%', md: '75%', lg: '95%' }}
-              style={{ overflow: 'hidden' }}
-              // position={{ initial: 'absolute', xs: 'absolute', md: 'relative', lg: 'relative' }}
-            >
-              <Flex
-                // style={{ padding: '2' }}
-                p={'2'}
-                wrap={'wrap'}
-                // columns={{ xs: '1', sm: '2', md: '2', lg: '2', xl: '2' }}
-                gap="2"
-                width="100%"
-                minWidth={'100%'}
-                maxWidth={'100%'}
-                maxHeight={'100%'}
-                // height={'100%'}
-                justify={'center'}
-                align={'center'}
-                style={{ margin: 'auto', overflow: 'hidden' }}
-                //  position={{ initial: 'absolute', xs: 'absolute', md: 'relative', lg: 'relative' }}
+          <div style={{ overflow: 'hidden', width: '100%' }}>
+            <AlertDialog.Root defaultOpen>
+              <AlertDialog.Content
+                width={{ initial: '100%', xs: '90%', sm: '90%', md: '75%', lg: '95%' }}
+                maxWidth={{ initial: '100%', xs: '90%', sm: '90%', md: '75%', lg: '95%' }}
+                minWidth={{ initial: '105%', xs: '90%', sm: '90%', md: '75%', lg: '95%' }}
+                maxHeight={{ initial: '100%', xs: '90%', sm: '90%', md: '75%', lg: '95%' }}
+                style={{ overflow: 'hidden' }}
+                // position={{ initial: 'absolute', xs: 'absolute', md: 'relative', lg: 'relative' }}
               >
-                {/* <LandingCaption /> */}
                 <Flex
-                  position={{ initial: 'absolute', xs: 'absolute', md: 'relative', lg: 'relative' }}
-                  width={{ initial: '100%', xs: '90%', sm: '90%', md: '40%', lg: '40%' }}
+                  // style={{ padding: '2' }}
+                  p={'2'}
+                  wrap={'wrap'}
+                  // columns={{ xs: '1', sm: '2', md: '2', lg: '2', xl: '2' }}
+                  gap="2"
+                  width="100%"
+                  minWidth={'100%'}
+                  maxWidth={'100%'}
+                  maxHeight={'100%'}
+                  // height={'100%'}
+                  justify={'center'}
+                  align={'center'}
+                  style={{ margin: 'auto', overflow: 'hidden' }}
+                  //  position={{ initial: 'absolute', xs: 'absolute', md: 'relative', lg: 'relative' }}
                 >
-                  <Avatar
-                    src="/assets/happy.jpg"
-                    fallback="/assets/happy.jpg"
-                    style={{
-                      maxHeight: '70vh',
-                      height: '100%',
-                      minHeight: '70vh',
-                      width: '100%',
-                      // backgroundImage: `url(${happyImg})`,
-                      backgroundPosition: 'center',
-                      backgroundSize: 'cover',
-                      position: 'relative',
-                      borderRadius: '20px'
+                  {/* <LandingCaption /> */}
+                  <Flex
+                    position={{
+                      initial: 'absolute',
+                      xs: 'absolute',
+                      md: 'relative',
+                      lg: 'relative'
                     }}
-                  />
-                  <div style={{ position: 'absolute', top: 20, left: 10 }}>
-                    <Slider buttonVisible={false} />
-                  </div>
+                    width={{ initial: '100%', xs: '90%', sm: '90%', md: '40%', lg: '40%' }}
+                  >
+                    <Avatar
+                      src="/assets/happy.jpg"
+                      fallback="/assets/happy.jpg"
+                      style={{
+                        maxHeight: '70vh',
+                        height: '100%',
+                        minHeight: '70vh',
+                        width: '100%',
+                        // backgroundImage: `url(${happyImg})`,
+                        backgroundPosition: 'center',
+                        backgroundSize: 'cover',
+                        position: 'relative',
+                        borderRadius: '20px'
+                      }}
+                    />
+                    <div style={{ position: 'absolute', top: 20, left: 10 }}>
+                      <Slider buttonVisible={false} />
+                    </div>
 
-                  <div style={{ position: 'absolute', bottom: 20, left: 10 }}>
-                    <Heading>
-                      <AnimateText texts={['JUSTCHAT', `Let's Connect`]} />
-                    </Heading>
-                  </div>
+                    <div style={{ position: 'absolute', bottom: 20, left: 10 }}>
+                      <Heading>
+                        <AnimateText texts={['JUSTCHAT', `Let's Connect`]} />
+                      </Heading>
+                    </div>
+                  </Flex>
+
+                  {/* <Flex */}
+                  {/* // position={{ initial: 'absolute' }} */}
+                  {/* // width={{ initial: '100%', xs: '90%', sm: '90%', md: '40%', lg: '50%' }} */}
+                  {/* > */}
+                  <Login />
+                  {/* </Flex> */}
                 </Flex>
-
-                {/* <Flex */}
-                {/* // position={{ initial: 'absolute' }} */}
-                {/* // width={{ initial: '100%', xs: '90%', sm: '90%', md: '40%', lg: '50%' }} */}
-                {/* > */}
-                <Login />
-                {/* </Flex> */}
-              </Flex>
-            </AlertDialog.Content>
-          </AlertDialog.Root>
+              </AlertDialog.Content>
+            </AlertDialog.Root>
+          </div>
         }
       />
       <Route
