4127ffbd1ed4139c77b819a53cf502c3775d28c7
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-11-29 00:09:18 +0100

    fix: fix user update socket notification

--- backend/config/socket.js
+++ backend/config/socket.js
@@ -493,7 +493,7 @@ const configureSocket = (app, corsOptions) => {
 
             if (updated) {
               const updatedUser = updated;
-              socket.emit('Updating', { updatedUser });
+              // socket.emit('Updating', { updatedUser });
               socket.broadcast.emit('Updating', { updatedUser });
               console.log('Updating from server');
             }
