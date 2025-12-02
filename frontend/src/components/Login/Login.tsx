9be217d634abe778873fbdf7a62011f4dfb09502
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-12-02 09:41:07 +0100

    fix: final tweak on login ui and profile update notifications

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -77,11 +77,12 @@ const Login: React.FC<LoginProps> = ({
       <Flex
         align="center"
         justify="between"
+        p={'2'}
         // width={'100%'}
-        style={{ width: 'min(95%, 1440px)', marginBottom: '4px', zIndex: 50 }}
+        style={{ width: 'min(100%, 1440px)', marginBottom: '4px', zIndex: 50 }}
       >
         <Heading size="4">JustChat</Heading>
-        <Flex align="center" gap="2">
+        <Flex align="center" gap="2" mr={'2'}>
           {/* <Text size="2" color="gray">
               Theme
             </Text> */}
